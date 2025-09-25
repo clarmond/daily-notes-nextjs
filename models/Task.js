5f196c7cd73a3fca47069058ab0286326e408ee6
Chad Armond <chad.l.armond@nasa.gov>
2025-09-25 15:30:51 -0500

    Added owner field

--- models/Task.js
+++ models/Task.js
@@ -2,6 +2,11 @@ import { Schema, model, models } from "mongoose";
 
 const TaskSchema = new Schema(
   {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
     text: {
       type: String,
       required: [true, "Text is required"],
