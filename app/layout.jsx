4614bb13a7f53c93b89152bc900d91e912b9a44c
Chad Armond <chad.l.armond@nasa.gov>
2026-01-15 09:08:39 -0600

    Added suppressHydrationWarning

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
