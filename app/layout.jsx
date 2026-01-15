003e42990510ff98ef1d90fe390a8887fed63b7f
Chad Armond <chad.l.armond@nasa.gov>
2026-01-15 09:02:15 -0600

    Fixed hydration error by re-ordering tag structure

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,33 +11,33 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ToastProvider>
-      <GlobalProvider>
-        <AuthProvider>
-          <html lang="en">
-          <head>
-            <link
-              href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
-              rel="stylesheet"
-              integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"
-              crossOrigin="anonymous"
-            />
-          </head>
-          <script
-            async
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
-            crossOrigin="anonymous"
-          ></script>
-          <body>
-            <Navbar />
-            <div className="main-content">
-              {children}
-            </div>
-          </body>
-        </html>
-        </AuthProvider>
-      </GlobalProvider>
-    </ToastProvider>
+    <html lang="en">
+      <head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"
+          crossOrigin="anonymous"
+        />
+        <script
+          async
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
+      <body>
+        <ToastProvider>
+          <GlobalProvider>
+            <AuthProvider>
+              <Navbar />
+              <div className="main-content">
+                {children}
+              </div>
+            </AuthProvider>
+          </GlobalProvider>
+        </ToastProvider>
+      </body>
+    </html>
   );
 }
